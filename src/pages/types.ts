3aa96b9c4533db3867b90fc561dfc9562ee9ad2d
Soňa Šturmová <140394613+sonaStu@users.noreply.github.com>
2023-10-25 21:34:15 +0200

    Revert "Revert "feat: rewrite getcko coins markets fetch"" (#12)

---
+++ src/pages/types.ts
@@ -0,0 +1,11 @@
+export type Coin = {
+       paprika_id: string;
+       gecko_id: string;
+       name: string;
+       symbol: string;
+       rank: number;
+       is_new: boolean;
+       is_active: boolean;
+       svg: string;
+       chartSvgIndex: number | null;
+};
