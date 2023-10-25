33384491c3bf8da930df9bb76f0cca9c63043198
Soňa Šturmová <140394613+sonaStu@users.noreply.github.com>
2023-10-25 21:33:12 +0200

    Revert "Revert "feat: rewrite getcko coins markets fetch""

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { Switch, Route, useParams } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { DataContext } from '../components/navbar/DataContext';
-import { DataProvider } from '../modules/DataProvider';
+
 
 import RankingsPage from './mainpages/RankingsPage';
 import CoinsPage from './mainpages/CoinsPage';
@@ -9,22 +9,19 @@ import About from './About';
 import { Loader } from '@mantine/core';
 
 import { Box } from '@mantine/core';
+import { fetchGeckoCoinsMarkets } from '../Fetches/coinGecko';
+import { getChartSvgIndex } from '../helpers';
+import { Coin } from './types';
 
-interface RouteParams {
-  id: string;
-}
 
-/************************************
- *
- * MainPage
- *
- * ******************************** */
+
+
 const MainPage: React.FC = (props) => {
   const { coinsInfos, setCoinsInfos } = useContext(DataContext);
   const [lastUpdateTime, setLastUpdateTime] = useState<Date>();
 
   const loading =
-    coinsInfos.list.length === 0 ? (
+    coinsInfos?.list?.length === 0 ? (
       <Box sx={{ display: 'flex', justifyContent: 'center' }}>
         <Loader />
       </Box>
@@ -33,7 +30,7 @@ const MainPage: React.FC = (props) => {
     );
 
   useEffect(() => {
-    if (coinsInfos.list.length === 0) {
+    if (coinsInfos?.list?.length === 0) {
       fetchCoinsList();
     }
   }, []);
@@ -43,14 +40,36 @@ const MainPage: React.FC = (props) => {
   };
 
   const fetchCoinsList = async () => {
-    await DataProvider.getCoinList().then((coins) => {
+    const { data } = await fetchGeckoCoinsMarkets({ currency: "usd" });
+
+    const promises = data?.map(async (coin) => {
+      const key = coin.symbol.toLowerCase();
+      const coinData: Coin = {
+        paprika_id: '',
+        gecko_id: coin?.id,
+        name: coin?.name,
+        symbol: coin?.symbol,
+        rank: coin?.market_cap_rank,
+        is_new: false,
+        is_active: true,
+        svg: coin?.image,
+        chartSvgIndex: getChartSvgIndex({ imageUrl: coin?.image }),
+      };
+      return [key, coinData] as [string, Coin];
+    });
+
+    if (promises) {
+      const coinEntries = await Promise.all(promises);
+      const filteredCoinEntries = coinEntries.filter(Boolean);
+      const coinList = new Map(filteredCoinEntries);
+
       setCoinsInfos(() => {
         const infos = {
-          list: coins,
+          list: coinList,
         };
         return infos;
       });
-    });
+    }
   };
 
   return (
