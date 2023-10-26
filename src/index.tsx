c45fcfee9ea9abccc783443e66194c449b0a3cda
sonaStu <s.sturmova@gmail.com>
2023-10-26 23:09:19 +0200

    analytics added

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { inject } from '@vercel/analytics';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
+inject();
